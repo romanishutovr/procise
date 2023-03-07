69dcf6a7afc8b488cca4fa808d8d0b7493e1888b
Yevhen Piotrovskyi <yevhen.piotrovskyi@yojji.io>
2023-03-06 19:07:26 -0600

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    "./content/_includes/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
+    "./content/blog/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
+    "./content/pages/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
+    "./content/index.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
+  ]
+}
