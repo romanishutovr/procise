31862263326537bb715871a50a045adc6decbef9
Yevhen Piotrovskyi <yevhen.piotrovskyi@yojji.io>
2023-03-09 14:42:22 -0600

    fix sitemap

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
 	"title": "Eleventy Base Blog v8",
-	"url": "https://example.com/",
+	"url": "http://localhost:8080/",
 	"language": "en",
 	"description": "I am writing about my experiences as a naval navel-gazer.",
 	"author": {
