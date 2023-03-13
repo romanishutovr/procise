bd894e8fda84fcac53981918f2c8208e380a5890
Yevhen Piotrovskyi <yevhen.piotrovskyi@yojji.io>
2023-03-13 15:42:50 -0500

    add langugaes

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,20 @@
 module.exports = {
-	"title": "Eleventy Base Blog v8",
-	"url": "http://localhost:8080/",
-	"language": "en",
-	"description": "I am writing about my experiences as a naval navel-gazer.",
-	"author": {
-		"name": "Your Name Here",
-		"email": "youremailaddress@example.com",
-		"url": "https://example.com/about-me/"
-	}
-}
+  title: "Eleventy Base Blog v8",
+  url: "http://localhost:8080/",
+  languages: [
+    {
+      code: "en",
+      text: "Global - English",
+    },
+    {
+      code: "de",
+      text: "Deutschland - Deutsch",
+    },
+  ],
+  description: "I am writing about my experiences as a naval navel-gazer.",
+  author: {
+    name: "Your Name Here",
+    email: "youremailaddress@example.com",
+    url: "https://example.com/about-me/",
+  },
+};
