88e31d09e0508a19b4f4f45e3c17b9cfc42e2457
Yevhen Piotrovskyi <yevhen.piotrovskyi@yojji.io>
2023-03-13 13:49:10 -0500

    cleanup

--- _data/pagesData.js
+++ _data/pagesData.js
@@ -1,29 +1,29 @@
-const fs = require('fs')
-const matter = require('gray-matter');
+const fs = require("fs");
+const matter = require("gray-matter");
 
-const langs = fs.readdirSync('./content').filter(filename => {
+const langs = fs.readdirSync("./content").filter((filename) => {
   // Filter out files
-  if (filename.indexOf('.') !== -1) return false
+  if (filename.indexOf(".") !== -1) return false;
 
   // Filter out non-ISO 639-1 codes
-  if (filename.length !== 2) return false
+  if (filename.length !== 2) return false;
 
-  return true
-})
+  return true;
+});
 
+const res = langs.reduce((acc, lang) => {
+  const langCustomPagesNames = fs
+    .readdirSync(`./content/${lang}`)
+    .filter((filename) => {
+      return filename.indexOf(".md") !== -1;
+    })
+    .map((filename) => {
+      const content = fs.readFileSync(`./content/${lang}/${filename}`, "utf8");
+      const parsed = matter(content);
+      return { ...parsed, page: filename };
+    });
 
-const res=  langs.reduce((acc, lang) => {
-  const langCustomPagesNames = fs.readdirSync(`./content/${lang}`)
-  .filter(filename => {
-    return filename.indexOf('.md') !== -1
-  }).map(filename => {
-    const content = fs.readFileSync(`./content/${lang}/${filename}`, 'utf8')
-    const parsed = matter(content)
-    return parsed
-  })
+  return { ...acc, [lang]: langCustomPagesNames };
+}, {});
 
-
-  return {...acc, [lang]: langCustomPagesNames}
-},{})
-
-module.exports = res
+module.exports = res;
