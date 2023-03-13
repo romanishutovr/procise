3146b61fa412592429d6a2c8b575e618cf2b3dcf
Yevhen Piotrovskyi <yevhen.piotrovskyi@yojji.io>
2023-03-13 13:47:26 -0500

    add pagesData

---
+++ _data/pagesData.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const matter = require('gray-matter');
+
+const langs = fs.readdirSync('./content').filter(filename => {
+  // Filter out files
+  if (filename.indexOf('.') !== -1) return false
+
+  // Filter out non-ISO 639-1 codes
+  if (filename.length !== 2) return false
+
+  return true
+})
+console.log(langs)
+
+
+const res=  langs.reduce((acc, lang) => {
+  const langCustomPagesNames = fs.readdirSync(`./content/${lang}`)
+  .filter(filename => {
+    return filename.indexOf('.md') !== -1
+  }).map(filename => {
+    const content = fs.readFileSync(`./content/${lang}/${filename}`, 'utf8')
+    const parsed = matter(content)
+    return parsed
+  })
+
+
+  return {...acc, [lang]: langCustomPagesNames}
+},{})
+
+console.log(res)
+module.exports = res
