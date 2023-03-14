176e825a5c489167686f996ec71615d3bf71dfd5
Roman <roman.ishutov@jetsoftpro.com>
2023-03-14 18:23:05 +0200

    save

--- _data/navProducts.js
+++ _data/navProducts.js
@@ -1,114 +1,43 @@
-module.exports = [
-  {
-    firstDropTitle: "The Clinic",
-    secondDrop: {
-      secondDropItems: [
-        {
-          secondDropTitle: "Stop the Bleeding",
-          lastDropTitle: "Stop the Bleeding",
-          lasDropDescription:
-            "Provide immediate assistance in identifying inefficiencies and implementing countermeasures to help eliminate shortcomings.",
-        },
-        {
-          secondDropTitle: "Intensive Care",
-          lastDropTitle: "Intensive Care",
-          lasDropDescription:
-            "Our intensive care unit provides the critical care and support for acutely poor processes and workflows.",
-        },
-        {
-          secondDropTitle: "X-Ray",
-          lastDropTitle: "X-Ray",
-          lasDropDescription:
-            "X-ray processes & workflows to find what's broken or could be improved",
-        },
-      ],
-    },
-  },
-  {
-    firstDropTitle: "The Workshop",
-    secondDrop: {
-      secondDropItems: [
-        {
-          secondDropTitle: "Find",
-          lastDropTitle: "Find",
-          lasDropDescription:
-            "X-ray processes & workflows to find what's broken or could be improved.",
-        },
-        {
-          secondDropTitle: "Frame",
-          lastDropTitle: "Frame",
-          lasDropDescription:
-            "Quantifying the value of what's broken. Understand and prioritize based on performance impact to suggest improvinements using best practices.",
-        },
-        {
-          secondDropTitle: "Fix",
-          lastDropTitle: "Fix",
-          lasDropDescription:
-            "Modernize legacy processes.Remediate exceptions and respond to critical business events without changing system.Enrich workflows across ServiceNow chatbots, portals, mobile devices, and workspaces to empower people to do more.",
-        },
-      ],
-    },
-  },
-  {
-    firstDropTitle: "The Journey",
-    secondDrop: {
-      secondDropItems: [
-        {
-          secondDropTitle: "Excite",
-          lastDropTitle: "Excite",
-          lasDropDescription:
-            "Build sponsorship, excitement, and hype for the change.",
-        },
-        {
-          secondDropTitle: "Enable",
-          lastDropTitle: "Enable",
-          lasDropDescription:
-            "Deploy services and ensure everyone is enabled on how to use the new platform.",
-        },
-        {
-          secondDropTitle: "Expand",
-          lastDropTitle: "Expand",
-          lasDropDescription:
-            "Establish a sponsorship team, run training workshops, and manage support operations.",
-        },
-        {
-          secondDropTitle: "Embed",
-          lastDropTitle: "Embed",
-          lasDropDescription:
-            "Measure adoption progress, promote success stories, and support users with self-help resources.",
-        },
-      ],
-    },
-  },
-  {
-    firstDropTitle: "The Reward",
-    secondDrop: {
-      secondDropItems: [
-        {
-          secondDropTitle: "Being aware",
-          lastDropTitle: "Being aware",
-          lasDropDescription:
-            "Use real-time, in-platform data as the single source of truth - not scattered sources of information.",
-        },
-        {
-          secondDropTitle: "Planning ahead",
-          lastDropTitle: "Planning ahead",
-          lasDropDescription:
-            "Get clarity on what matters and quickly redirect service coverage to where it's needed most.",
-        },
-        {
-          secondDropTitle: "Being prepared",
-          lastDropTitle: "Being prepared",
-          lasDropDescription:
-            "Monitor performance to identify areas for improvement and detect service bottlenecks before they occur.",
-        },
-        {
-          secondDropTitle: "Continual Improvement",
-          lastDropTitle: "Continual Improvement",
-          lasDropDescription:
-            "Take action on KPIs using time charts, forecasts, breakdowns, and dashboards.",
-        },
-      ],
-    },
-  },
-];
+const fs = require("fs");
+const matter = require("gray-matter");
+
+const langs = fs.readdirSync("./content").filter((filename) => {
+  // Filter out files
+  if (filename.indexOf(".") !== -1) return false;
+
+  // Filter out non-ISO 639-1 codes
+  if (filename.length !== 2) return false;
+
+  return true;
+});
+const groupProducts = [];
+
+const res = langs.reduce((acc, lang) => {
+  const langCustomPagesNames = fs
+    .readdirSync(`./content/${lang}`)
+    .filter((filename) => {
+      return filename.indexOf(".md") !== -1;
+    })
+    .forEach((filename, idx) => {
+      const content = fs.readFileSync(`./content/${lang}/${filename}`, "utf8");
+      const parsed = matter(content);
+      if (parsed.data.group === "product") {
+        const temp = [];
+        parsed.data.contentSections.forEach((item) => {
+          temp.push({
+            secondDropTitle: item.title,
+            lastDropTitle: item.title,
+            lasDropDescription: item.subtitle,
+          });
+        });
+
+        groupProducts.push({
+          pageLink: filename.slice(0, -3), // remove .md from end
+          firstDropTitle: parsed.data.pageName,
+          secondDrop: { secondDropItems: temp },
+        });
+      }
+    });
+});
+
+module.exports = groupProducts;
--- _data/navSolutions.js
+++ _data/navSolutions.js
@@ -1,169 +1,43 @@
-module.exports = [
-  {
-    firstDropTitle: "Workflow Excellence",
-    secondDrop: {
-      secondDropItems: [
-        {
-          secondDropTitle: "Optimization",
-          lastDropTitle: "Optimization",
-          lasDropDescription:
-            "Deliver better experiences for developers, site reliability engineers (SREs), and customers with our cloud-native reliability platform. Learn more about Lightstep from ServiceNow.",
-        },
-        {
-          secondDropTitle: "Standardization",
-          lastDropTitle: "Standardization",
-          lasDropDescription:
-            "Only ServiceNow can bring your solutions to life with the power of the Now Platform®. Make technology service delivery resilient, cost-efficient, and secure across your organization.",
-        },
-        {
-          secondDropTitle: "Compliance",
-          lastDropTitle: "Compliance",
-          lasDropDescription:
-            "Deliver better experiences for developers, site reliability engineers (SREs), and customers with our cloud-native reliability platform. Learn more about Lightstep from ServiceNow.",
-        },
-        {
-          secondDropTitle: "Metrics & KPIs",
-          lastDropTitle: "Metrics & KPIs",
-          lasDropDescription:
-            "Only ServiceNow can bring your solutions to life with the power of the Now Platform®. Make technology service delivery resilient, cost-efficient, and secure across your organization.",
-        },
-      ],
-    },
-  },
-  {
-    firstDropTitle: "Customer Service",
-    secondDrop: {
-      secondDropItems: [
-        {
-          secondDropTitle: "Customer Experience",
-          lastDropTitle: "Customer Experience",
-          lasDropDescription:
-            "Deliver better experiences for developers, site reliability engineers (SREs), and customers with our cloud-native reliability platform. Learn more about Lightstep from ServiceNow.",
-        },
-        {
-          secondDropTitle: "Customer Centric Workflows",
-          lastDropTitle: "Customer Centric Workflows",
-          lasDropDescription:
-            "Only ServiceNow can bring your solutions to life with the power of the Now Platform®. Make technology service delivery resilient, cost-efficient, and secure across your organization.",
-        },
-        {
-          secondDropTitle: "Self Service",
-          lastDropTitle: "Self Service",
-          lasDropDescription:
-            "Deliver better experiences for developers, site reliability engineers (SREs), and customers with our cloud-native reliability platform. Learn more about Lightstep from ServiceNow.",
-        },
-        {
-          secondDropTitle: "Mobile First",
-          lastDropTitle: "Mobile First",
-          lasDropDescription:
-            "Only ServiceNow can bring your solutions to life with the power of the Now Platform®. Make technology service delivery resilient, cost-efficient, and secure across your organization.",
-        },
-      ],
-    },
-  },
-  {
-    firstDropTitle: "Technology Excellence",
-    secondDrop: {
-      secondDropItems: [
-        {
-          secondDropTitle: "Optimize Operations",
-          lastDropTitle: "Optimize & Automate Techn. Service Operations",
-          lasDropDescription:
-            "Deliver better experiences for developers, site reliability engineers (SREs), and customers with our cloud-native reliability platform. Learn more about Lightstep from ServiceNow.",
-        },
-        {
-          secondDropTitle: "Strategize your Portfolio",
-          lastDropTitle: "Optimize Technology Asset Investment",
-          lasDropDescription:
-            "Only ServiceNow can bring your solutions to life with the power of the Now Platform®. Make technology service delivery resilient, cost-efficient, and secure across your organization.",
-        },
-        {
-          secondDropTitle: "Deliver Intitiatives in time",
-          lastDropTitle: "Deliver strategic Technology Initiatives",
-          lasDropDescription:
-            "Deliver better experiences for developers, site reliability engineers (SREs), and customers with our cloud-native reliability platform. Learn more about Lightstep from ServiceNow.",
-        },
-        {
-          secondDropTitle: "Scrum / Agile",
-          lastDropTitle: "Scrum / Agile",
-          lasDropDescription:
-            "Only ServiceNow can bring your solutions to life with the power of the Now Platform®. Make technology service delivery resilient, cost-efficient, and secure across your organization.",
-        },
-        {
-          secondDropTitle: "SAFe",
-          lastDropTitle: "SAFe",
-          lasDropDescription:
-            "Deliver better experiences for developers, site reliability engineers (SREs), and customers with our cloud-native reliability platform. Learn more about Lightstep from ServiceNow.",
-        },
-      ],
-    },
-  },
-  {
-    firstDropTitle: "Operational Excellence ",
-    secondDrop: {
-      secondDropItems: [
-        {
-          secondDropTitle: "Workflow & Valuestream Analysis",
-          lastDropTitle: "Workflow & Valuestream Analysis",
-          lasDropDescription:
-            "Deliver better experiences for developers, site reliability engineers (SREs), and customers with our cloud-native reliability platform. Learn more about Lightstep from ServiceNow.",
-        },
-        {
-          secondDropTitle: "Manage your Risk",
-          lastDropTitle: "Manage Risk & Resilience",
-          lasDropDescription:
-            "Only ServiceNow can bring your solutions to life with the power of the Now Platform®. Make technology service delivery resilient, cost-efficient, and secure across your organization.",
-        },
-        {
-          secondDropTitle: "Activate ESG Across the Enterprise",
-          lastDropTitle: "Activate ESG across the Enterprise",
-          lasDropDescription:
-            "Deliver better experiences for developers, site reliability engineers (SREs), and customers with our cloud-native reliability platform. Learn more about Lightstep from ServiceNow.",
-        },
-        {
-          secondDropTitle: "Metrics & KPIs",
-          lastDropTitle: "Metrics & KPIs",
-          lasDropDescription:
-            "Only ServiceNow can bring your solutions to life with the power of the Now Platform®. Make technology service delivery resilient, cost-efficient, and secure across your organization.",
-        },
-        {
-          secondDropTitle: "Design Thinking & Sprints",
-          lastDropTitle: "Design Thinking & Sprints",
-          lasDropDescription:
-            "Deliver better experiences for developers, site reliability engineers (SREs), and customers with our cloud-native reliability platform. Learn more about Lightstep from ServiceNow.",
-        },
-      ],
-    },
-  },
-  {
-    firstDropTitle: "Intelligent Automation",
-    secondDrop: {
-      secondDropItems: [
-        {
-          secondDropTitle: "Automate and Connect Anything",
-          lastDropTitle: "Automate and Connect Anything to ServiceNow",
-          lasDropDescription:
-            "Deliver better experiences for developers, site reliability engineers (SREs), and customers with our cloud-native reliability platform. Learn more about Lightstep from ServiceNow.",
-        },
-        {
-          secondDropTitle: "Creator Workflows",
-          lastDropTitle: "Creator Workflows",
-          lasDropDescription:
-            "Only ServiceNow can bring your solutions to life with the power of the Now Platform®. Make technology service delivery resilient, cost-efficient, and secure across your organization.",
-        },
-        {
-          secondDropTitle: "Service Prediction",
-          lastDropTitle: "Service Prediction",
-          lasDropDescription:
-            "Deliver better experiences for developers, site reliability engineers (SREs), and customers with our cloud-native reliability platform. Learn more about Lightstep from ServiceNow.",
-        },
-        {
-          secondDropTitle: "Predictive Maintenance",
-          lastDropTitle: "Predictive Maintenance",
-          lasDropDescription:
-            "OptiOnly ServiceNow can bring your solutions to life with the power of the Now Platform®. Make technology service delivery resilient, cost-efficient, and secure across your organization.mization4",
-        },
-      ],
-    },
-  },
-];
+const fs = require("fs");
+const matter = require("gray-matter");
+
+const langs = fs.readdirSync("./content").filter((filename) => {
+  // Filter out files
+  if (filename.indexOf(".") !== -1) return false;
+
+  // Filter out non-ISO 639-1 codes
+  if (filename.length !== 2) return false;
+
+  return true;
+});
+const groupSolutions = [];
+
+const res = langs.reduce((acc, lang) => {
+  const langCustomPagesNames = fs
+    .readdirSync(`./content/${lang}`)
+    .filter((filename) => {
+      return filename.indexOf(".md") !== -1;
+    })
+    .forEach((filename, idx) => {
+      const content = fs.readFileSync(`./content/${lang}/${filename}`, "utf8");
+      const parsed = matter(content);
+      if (parsed.data.group === "solution") {
+        const temp = [];
+        parsed.data.contentSections.forEach((item) => {
+          temp.push({
+            secondDropTitle: item.title,
+            lastDropTitle: item.title,
+            lasDropDescription: item.subtitle,
+          });
+        });
+
+        groupSolutions.push({
+          pageLink: filename.slice(0, -3), // remove .md from end
+          firstDropTitle: parsed.data.pageName,
+          secondDrop: { secondDropItems: temp },
+        });
+      }
+    });
+});
+
+module.exports = groupSolutions;
