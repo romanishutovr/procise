37ac5d6fdba7c7f671268569936e245cf20f4a70
Roman Ishutov <92449059+romanishutovr@users.noreply.github.com>
2023-03-18 00:10:42 +0200

    Optimization (#2)
    * initial implementation for responsive images
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * update arrow lang
    
    * update index
    
    * save
    
    * add webp, update index
    
    * update size
    
    * add lazy
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * fix drop
    
    * fix footer
    
    ---------
    
    Co-authored-by: Yevhen Piotrovskyi <yevhen.piotrovskyi@yojji.io>
    Co-authored-by: Roman <roman.ishutov@jetsoftpro.com>

---
+++ compress.js
@@ -0,0 +1,37 @@
+const fs = require("fs").promises;
+const sharp = require("sharp");
+
+module.exports = async function main() {
+  console.log("start");
+  const outputPath = "_site/assets/images/";
+  const widthList = [640, 828, 1200, 2048];
+  const startTs = Date.now();
+  const imagesList = await fs.readdir("./public/assets/images/"); // add checks
+
+  for (let index = 0; index < imagesList.length; index++) {
+    const imagePath = imagesList[index];
+    const extenstion = imagePath.split(".").pop();
+    const imageBaseName = imagePath.split(".").slice(0, -1).join(".");
+    // const image = await fs.readFile(`./public/assets/images/${imagePath}`);
+
+    for (let j = 0; j < widthList.length; j++) {
+      const currentWidth = widthList[j];
+
+      const compressedBuffer = await sharp(
+        `./public/assets/images/${imagePath}`
+      )
+        .webp()
+        .resize(currentWidth)
+        // .jpeg({ mozjpeg: true }) // what is mozjpeg?
+        .toFile(`${outputPath}${imageBaseName}_width-${currentWidth}.webp`);
+
+      // await fs.writeFile(
+      //   `${outputPath}${imageBaseName}_width-${currentWidth}.${extenstion}`,
+      //   compressedBuffer
+      // );
+    }
+  }
+
+  const endTs = Date.now();
+  console.log(`Compressed ${imagesList.length} images in ${endTs - startTs}ms`);
+};
