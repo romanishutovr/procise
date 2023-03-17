235e2edb8a636aaf132980a0f470ddf9f640c473
Roman <roman.ishutov@jetsoftpro.com>
2023-03-17 15:03:41 +0200

    save

--- compress.js
+++ compress.js
@@ -1,10 +1,10 @@
 const fs = require("fs").promises;
 const sharp = require("sharp");
-const outputPath = "_site/assets/images/";
 
-const widthList = [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000];
-
-async function main() {
+module.exports = async function main() {
+  console.log("start");
+  const outputPath = "_site/assets/images/";
+  const widthList = [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000];
   const startTs = Date.now();
   const imagesList = await fs.readdir("./public/assets/images/"); // add checks
 
@@ -17,7 +17,9 @@ async function main() {
     for (let j = 0; j < widthList.length; j++) {
       const currentWidth = widthList[j];
 
-      const compressedBuffer = await sharp(`./public/assets/images/${imagePath}`)
+      const compressedBuffer = await sharp(
+        `./public/assets/images/${imagePath}`
+      )
         .rotate()
         .resize(currentWidth)
         .jpeg({ mozjpeg: true }) // what is mozjpeg?
@@ -32,6 +34,4 @@ async function main() {
 
   const endTs = Date.now();
   console.log(`Compressed ${imagesList.length} images in ${endTs - startTs}ms`);
-}
-
-main();
+};
